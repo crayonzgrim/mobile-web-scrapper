81f36bfd6235ca6a0b59b7611e9c8513851ca2ce
KIM DONG IL <crayonzgrim@gmail.com>
2025-03-23 17:58:48 +0900

    🔨 AddLink screen

---
+++ components/shared/SingleLineInput.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { TextInput, View } from 'react-native';
+
+type SingleLineInputProps = {
+  value: string;
+  onChangeText: (text: string) => void;
+  placeholder: string;
+}
+
+export const SingleLineInput = ({ value, onChangeText, placeholder }: SingleLineInputProps) => {
+  const [focused, setFocused] = useState(false);
+
+  return (
+    <View style={{
+      alignSelf: 'stretch',
+      paddingHorizontal: 12,
+      paddingVertical: 8,
+      borderRadius: 4,
+      borderWidth: 1,
+      borderColor: focused ? 'black' : 'gray'
+    }}>
+      <TextInput
+        autoCorrect={false}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+
+      />
+
+    </View>
+  )
+};
